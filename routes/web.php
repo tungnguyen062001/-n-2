9c5869c90b2a453ae5863c86b4a488274bd7e9fe
tungnguyen062001 <76606320+tungnguyen062001@users.noreply.github.com>
2021-12-01 21:02:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('user/list', function () {
     return view('user.list');
 });
-Route::get('login/login', function () {
-    return view('login.login');
-});
-Route::get('user/create', function () {
-    return view('user.create');
-});
-Route::get('diemdanh/diemdanh', function () {
-    return view('diemdanh.diemdanh');
-});
-Route::get('sinhvien/list', function () {
-    return view('sinhvien.list');
-});
-Route::get('giaodien/giaodien', function () {
-    return view('giaodien.giaodien');
-});
-Route::get('lop/list', function () {
-    return view('lop.list');
-});
+// Route::get('login/login', function () {
+//     return view('login.login');
+// });
+// Route::get('user/create', function () {
+//     return view('user.create');
+// });
+// Route::get('diemdanh/diemdanh', function () {
+//     return view('diemdanh.diemdanh');
+// });
+// Route::get('sinhvien/list', function () {
+//     return view('sinhvien.list');
+// });
+// Route::get('giaodien/giaodien', function () {
+//     return view('giaodien.giaodien');
+// });
+// Route::get('lop/list', function () {
+//     return view('lop.list');
+// });
