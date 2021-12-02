436a9d6fdbfcfd2d5147fec9ffbca5f0ef06eef9
sinoneko <76605930+sinoneko@users.noreply.github.com>
2021-12-02 17:07:39 +0700

    hu

--- database/migrations/2021_09_15_035205_create_lop_BKD05_table.php
+++ database/migrations/2021_09_15_035205_create_lop_BKD05_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLopBKD05Table extends Migration
+class CreateLopBKD05Table extends migrations
 {
     /**
      * Run the migrations.
@@ -16,7 +16,7 @@ class CreateLopBKD05Table extends Migration
         Schema::create('lop_BKD05', function (Blueprint $table) {
             $table->integer('id_sinh_vien');
             $table->string('ten-lop',50);
-            
+
             $table->foreign('id_sinh_vien')->references('id_sinh_vien')->on('sinh_vien');
             $table->foreign('ten_lop')->references('ten_lop')->on('lop');
         });
